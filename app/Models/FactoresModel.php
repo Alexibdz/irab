349a9faaddfd7258b2c0f0112d9ce52bcc94cf0f
alexibdz <alexisangelartaza@gmail.com>
2026-09-07 19:16:12 -0300

    Creacion de BaseModel. Optimizacion de lineas de codigo.

--- app/Models/FactoresModel.php
+++ app/Models/FactoresModel.php
@@ -2,41 +2,9 @@
 
 namespace App\Models;
 
-use CodeIgniter\Model;
-
-class FactoresModel extends Model
+class FactoresModel extends BaseModel
 {
-    protected $table            = 'factores';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = true;
+    protected $table = 'factores';
 
-    protected $allowedFields = ['denominacion','tipo','tipo_formulario','fecha_registro','fecha_edicion','fecha_borrado'];
-    protected bool $allowEmptyInserts = false;
-    protected bool $updateOnlyChanged = true;
-
-    // Dates
-    protected $useTimestamps = true;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'fecha_registro';
-    protected $updatedField  = 'fecha_edicion';
-    protected $deletedField  = 'fecha_borrado';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    protected $allowedFields = ['denominacion','tipo','tipo_formulario','fecha_borrado'];
 }
--- app/Models/PacienteModel.php
+++ app/Models/PacienteModel.php
@@ -1,26 +1,17 @@
 <?php
-namespace App\Models;
-use CodeIgniter\Model;
 
-class PacienteModel extends Model {
+namespace App\Models;
+
+class PacienteModel extends BaseModel
+{
     protected $table = 'pacientes';
-    
-    protected $primaryKey = 'id';
 
     protected $allowedFields = [
-        'dni', 
-        'nombre', 
+        'dni',
+        'nombre',
         'fecha_nacimiento',
         'id_tutor', 
         'id_establecimiento_habitual',
         'fecha_borrado'
     ];
-
-    // para usar elborrado lógico
-    protected $useTimestamps   = true;
-    protected $useSoftDeletes  = true;
-
-    protected $createdField    = 'fecha_registro';
-    protected $updatedField    = 'fecha_edicion';
-    protected $deletedField    = 'fecha_borrado';
 }
